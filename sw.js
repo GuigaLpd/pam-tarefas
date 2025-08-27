85d9f8b5dedaea0766ef28ff80f3d4abf31b6a94
GuigaLpd <guilhermecampos0108@gmail.com>
2025-08-27 20:31:16 -0300

    update

--- sw.js
+++ sw.js
@@ -1,7 +1,18 @@
+const CACHE_NAME = "tarefas";
+const FILES_TO_CACHE = [
+  '/pam-tarefas/',
+  '/pam-tarefas/index.html',
+  '/pam-tarefas/style.css',
+  '/pam-tarefas/script.js',
+  '/pam-tarefas/manifest.json',
+  '/pam-tarefas/imgs/img.png',
+  '/pam-tarefas/imgs/img2.png'
+];
+
 self.addEventListener("install", (event) => {
     caches.open("tarefas").then((cache) => {
         console.log("Cache aberto")
-        cache.addAll();
+        cache.addAll(FILES_TO_CACHE);
     });
 });
 
